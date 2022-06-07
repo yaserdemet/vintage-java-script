d3698e73486e61ca9c65106c90fcf1a58306faae
yaser <ddemety372@gmail.com>
2022-06-07 21:38:41 +0300

    part of js

---
+++ checkout.js
@@ -0,0 +1,61 @@
+const taxRate = 0.18;
+const shippingPrice = 15.0;
+
+window.addEventListener("load", () => {
+    localStorage.setItem("taxRate", taxRate);
+    localStorage.setItem("shippingPrice", shippingPrice);
+    sessionStorage.setItem("taxRate", taxRate);
+    sessionStorage.setItem("shippingPrice", shippingPrice);
+    //add after func. coding
+    calculateCartTotal()
+});
+
+//capturing vs. bubbling
+let productsDiv = document.querySelector(".products");
+productsDiv.addEventListener("click", (e) => {
+    //console.log(event.target);
+    //minus buttons
+    if (e.target.classList.contains("minus")) {
+        // console.log("minusBtn clicked");
+        if (e.target.nextElementSibling.innerText > 1) {
+               // e.target.parentElement.lastElementChild.innerText;
+            e.target.nextElementSibling.innerText--;
+            //calculate Product and Cart Total
+            //passing selectedProductInfo as parameter 
+            calculateProductAndCartTotal(e.target.parentElement.parentElement);
+        }
+        else {
+            if (confirm("Product will be removed!")) {
+                e.target.parentElement.parentElement.parentElement.remove();
+                //calculateCartTotal
+                calculateCartTotal();
+            }
+        }
+    }
+
+    //plus buttons
+    else if (e.target.className == "plus") {
+        // console.log("plusBtn clicked");
+        // e.target.parentElement.firstElementChild.innerText;
+        e.target.previousElementSibling.innerText++;
+        //calculate Product and Cart Total
+        calculateProductAndCartTotal(e.target.parentElement.parentElement);
+    }
+
+    //remove buttons
+    else if (e.target.className == "remove-product") {
+        // console.log("removeBtn clicked");
+        if (confirm("Product will be removed")) {
+            e.target.parentElement.parentElement.parentElement.remove();
+                    //calculateCartTotal
+            calculateCartTotal()
+        }
+
+    }
+    //other elements
+    else {
+        console.log("other elements clicked");
+    }
+})
+
+
